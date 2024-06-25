ab68998758e0fb2b1de13cb58a879ec2c9791e23
Daniel <danielchenschw@gmail.com>
2024-06-25 18:04:54 -0400

    Made Header More Responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,8 @@ function hamburgerHandler() {
 
 function Header() {
     return (
-        <header className="Header flex flex-row justify-between w-screen pl-[22vw] pr-[22vw] h-[calc(4rem+4px)] p-8 items-center border-[828282] border">
+        <header className="Header flex flex-row justify-center items-center w-screen h-[calc(4rem+4px)] border-[828282] border">
+        <div className="headerContent w-screen xl:w-[60vw] p-8 flex flex-row justify-between items-center">
             <img src={logo} alt="Palette Pal Logo" className="logo h-10 w-auto md:h-14" />
 
             {/* nav bar on bigger screens */}
@@ -28,6 +29,7 @@ function Header() {
                 <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                 </svg>
+            </div>
             </div>
         </header>
     );
