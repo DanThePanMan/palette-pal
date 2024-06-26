aa429b9433eff99c52dc9fd21fe7040d28bf0d59
Daniel <danielchenschw@gmail.com>
2024-06-26 15:44:16 -0400

    Allow Palette Fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const data = {
+        method: "POST",
+        body: JSON.stringify({
+            model: "default",
+            input: [[44, 43, 44], [90, 83, 82], "N", "N", "N"],
+        }),
+    };
+
+    async function fetchPalette(params) {
+        try {
+            const response = await fetch("http://colormind.io/api/", params);
+            if (!response.ok) {
+                throw new Error("Could not get response from server");
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.log("error"); //make this show a modal
+        }
+    }
+
+    const [palette, setPalette] = useState(fetchPalette(data));
 
     return (
         <>
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
+            <button onClick={() => console.log(fetchPalette(data))}>
+                click me
             </button>
         </>
     );
