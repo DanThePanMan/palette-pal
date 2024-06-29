c70109c29e5e55dc653a5ffb3df772f31d51d1c4
Daniel <danielchenschw@gmail.com>
2024-06-28 22:49:53 -0400

    Add Hover Effects for Color Code Copying

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -8,13 +8,19 @@ function convertStyle(colorArray){
     })
 }
 
-function Palette(props){
+
+function Palette(){
 
     const [palette, setPalette] = useContext(paletteContext);
 
 
     return(
         <div className="w-[750px] h-[430px] bg-green-400 flex flex-row rounded-3xl overflow-clip border borde-[828282] relative">
+            <div className="w-[150px] h-[430px] hover:bg-black/30 absolute transition-all ease-in-out"></div>
+            <div className="w-[150px] h-[430px] hover:bg-black/30 absolute translate-x-[149px] transition-all ease-in-out"></div>
+            <div className="w-[150px] h-[430px] hover:bg-black/30 absolute translate-x-[298px] transition-all ease-in-out "></div>
+            <div className="w-[150px] h-[430px] hover:bg-black/30 absolute translate-x-[448px] transition-all ease-in-out"></div>
+            <div className="w-[150px] h-[430px] hover:bg-black/30 absolute translate-x-[598px] transition-all ease-in-out"></div>
             <div id = {0} className="w-[150px] h-[430px] transition-all ease-in-out" style={convertStyle(palette.lightShades)}></div>
             <div id = {1} className="w-[150px] h-[430px] transition-all ease-in-out" style={convertStyle(palette.lightAccent)}></div>
             <div id = {2} className="w-[150px] h-[430px] transition-all ease-in-out" style={convertStyle(palette.mainBrand)}></div>
