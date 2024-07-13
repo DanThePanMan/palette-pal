c5f50afed18274eae42094f1c73ba819da5ad7fe
Daniel <danielchenschw@gmail.com>
2024-07-12 21:43:38 -0400

    Add Dropdown To Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
 import logo from "../assets/Logo.png";
-
-//can remove
-import lightLogo from "../assets/LightLogo.png"
-
-function dropdownHandler() {
-    console.log("dropdown menu dropped");
-}
+import { useState } from "react";
+import HeaderDropdown from "./HeaderDropdown";
 
 
 function Header(props) {
+
+    const [dropdown, setDropdown] = useState(false);
+
     function hamburgerHandler() {
         props.setSidebar(true);
     }
+
+    function dropdownEnter(){
+        console.log("enter")
+        setDropdown(true);
+    }   
+
     
 
     
@@ -19,14 +23,17 @@ function Header(props) {
         <header className="Header flex flex-row justify-center items-center w-[100%] h-[calc(4rem+4px)] border-b border-[#dad9d9] bg-[#FFFFFF]">
         <div className="headerContent w-screen xl:w-[60vw] p-8 flex flex-row justify-between items-center">
             <a href="/"><img src={logo} alt="Palette Pal Logo" className="logo h-10 w-auto md:h-14" /></a>
+            {dropdown ? <HeaderDropdown setDropdown={setDropdown}/> : null}
 
             {/* nav bar on bigger screens */}
             <nav>
+            
                 <ul className="md:flex flex-row justify-around gap-10 text-md hidden font-medium text-[#2D2D2D]">
-                    <li><a href="#" className="text-[#2D2D2D] hover:text-[#828282] transition-all ease-in-out">Upcoming Features</a></li>
-                    <li><a href="#" className="text-[#2D2D2D] hover:text-[#828282] transition-all ease-in-out" onClick={dropdownHandler}>Tools</a>
+                    
+                    <li><a href="" className="text-[#2D2D2D] hover:text-[#828282] transition-all ease-in-out">Upcoming Features</a></li>
+                    <li><a href="" className="text-[#2D2D2D] hover:text-[#828282] transition-all ease-in-out" onMouseEnter={dropdownEnter} >Tools</a>
                     </li>
-                    <li><a href="#" className="text-[#2D2D2D] hover:text-[#828282] transition-all ease-in-out" >My Socials</a></li>
+                    <li><a href="" className="text-[#2D2D2D] hover:text-[#828282] transition-all ease-in-out" >My Socials</a></li>
                 </ul>
             </nav>
 
--- src/components/HeaderDropdown.jsx
+++ src/components/HeaderDropdown.jsx
@@ -1,3 +1,21 @@
-function headerDropdown(){
-    return(<div></div>);
+function HeaderDropdown(props){
+    function closeDropdown(){
+        props.setDropdown(false);
+    }
+
+
+
+    return(
+    <div >
+            <ul className="relative w-54 h-fit flex flex-col border bg-[#2D2D2D] z-50 bottom-4 rounded-sm left-[36vw] xl:left-[27vw] top-24 p-8 gap-4 mt-20" onMouseEnter={() => {console.log("test")}} onMouseLeave={() => closeDropdown()}>
+                <li><a href="#" className="text-white hover:text-[#828282] transition-all ease-in-out" >Palette Generator</a></li>
+                <li><a href="#" className="text-white hover:text-[#828282] transition-all ease-in-out" >Palete Preview</a></li>
+                <li><a href="#" className="text-white hover:text-[#828282] transition-all ease-in-out">Export to Bootstrap</a></li>
+                <li><a href="#" className="text-white hover:text-[#828282] transition-all ease-in-out">Export to Tailwind</a>
+                </li>
+                
+            </ul>
+    </div>);
 }
+
+export default HeaderDropdown
