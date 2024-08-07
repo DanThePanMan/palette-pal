003e035b4e62c1695b56c539b846d3f99731555d
Daniel <danielchenschw@gmail.com>
2024-08-06 22:23:16 -0400

    Create FirstFour.jsx

---
+++ src/pages/Previews/PreviewPages/PreviewBlog/BlogIndex/FirstFour.jsx
@@ -0,0 +1,4 @@
+import { convertStyleDiv } from "../../../Convert";
+import { convertStyleText } from "../../../Convert";
+import { paletteContext } from "../../../../CnR";
+import { useContext } from "react";
