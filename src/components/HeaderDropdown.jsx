60e715db0d9b9f6944d86bae36cc1f372e246383
Daniel <danielchenschw@gmail.com>
2024-08-20 19:25:52 -0400

    Remove Tailwind From Dropdown

--- src/components/HeaderDropdown.jsx
+++ src/components/HeaderDropdown.jsx
@@ -7,13 +7,10 @@ function HeaderDropdown(props){
 
     return(
     <div >
-            <ul className="relative w-54 rounded-xl h-fit flex flex-col border bg-[#2D2D2D] z-50 bottom-4 left-[36vw] xl:left-[27vw] top-24 p-6 gap-6 mt-28" onMouseLeave={() => closeDropdown()}>
+            <ul className="relative w-54 rounded-xl h-fit flex flex-col border bg-[#2D2D2D] z-50 bottom-4 left-[36vw] xl:left-[27vw] top-24 p-6 gap-6 mt-14" onMouseLeave={() => closeDropdown()}>
                 <li><div className=""><a href="/picker" className="text-white hover:text-[#828282] transition-all ease-in-out" >Palette Generator</a></div></li>
                 <li><a href="/preview" className="text-white hover:text-[#828282] transition-all ease-in-out" >Palete Preview</a></li>
                 <li><a href="/bootstrap" className="text-white hover:text-[#828282] transition-all ease-in-out">Export to Bootstrap</a></li>
-                <li><a href="#" className="text-white hover:text-[#828282] transition-all ease-in-out">Export to Tailwind</a>
-                </li>
-                
             </ul>
     </div>);
 }
