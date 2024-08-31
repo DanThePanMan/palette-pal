3af94bd47e415b7fc2d6af26f31ade4d999948ea
Daniel <danielchenschw@gmail.com>
2024-08-30 21:42:20 -0400

    Redo Verification

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,16 @@ import colors from "./assets/Colors.json";
 
 import "@fontsource-variable/inter"; // Defaults to wght axis
 
+export const metadata= {
+    title: "Palette Pal",
+    description: "Palette Preview Website for Developers",
+    verification: {
+        google: "hqFWAE_WkJ72t_igZExyr70EomLrstuM1Q9SHD7d5cI",
+    },
+};
+
+
 if (sessionStorage.getItem("defaultData") === null) {
-    
     sessionStorage.setItem("defaultData", JSON.stringify(colors));
     console.log("set default");
 }
