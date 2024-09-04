186580d722b912be46b1c6f2e0f8b0c39c9326c2
Daniel <danielchenschw@gmail.com>
2024-09-04 13:12:05 -0400

    Stop Footer From Overlapping Picker

--- src/pages/Picker/Picker.jsx
+++ src/pages/Picker/Picker.jsx
@@ -43,7 +43,7 @@ function Picker() {
     }
 
     return (
-        <main className="flex flex-col justify-around items-center w-[100%] md:m-24 md:mt-8 m-10 h-[60vh] gap-10 xl:gap-12 md:h-[calc(100vh-19rem)]">
+        <main className="flex flex-col justify-around items-center w-[100%] md:m-24 md:mt-8 m-10 h-[60vh] gap-10 xl:gap-12 md:h-[70vh]">
             <h1 className="font-bold text-xl md:text-3xl xl:text-5xl ">
                 Palette Generator
             </h1>
