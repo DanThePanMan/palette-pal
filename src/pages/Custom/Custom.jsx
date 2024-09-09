0c49019097b738825b0c81258e13d5486b755635
Daniel <danielchenschw@gmail.com>
2024-09-09 12:20:11 -0400

    Fix Custom Palette Overlapping With Footer

--- src/pages/Custom/Custom.jsx
+++ src/pages/Custom/Custom.jsx
@@ -3,7 +3,7 @@ import ButtonNoColor from "../../components/ButtonNoColor";
 
 function Custom() {
     return (
-        <main className="flex flex-col justify-start items-center w-[100%] h-screen md:mt-8 m-10 gap-6 md:gap-20 lg:gap-32 md:h-[calc(100vh-19rem)]">
+        <div className="flex flex-col justify-start items-center w-[100%] h-[120] md:mt-8 m-10 gap-6 md:gap-20 lg:gap-28">
             <h1 className="font-bold text-xl md:text-3xl xl:text-5xl ">
                 Custom Palette Preview
             </h1>
@@ -11,7 +11,7 @@ function Custom() {
             <a href="Preview">
                 <ButtonNoColor text="Preview"></ButtonNoColor>
             </a>
-        </main>
+        </div>
     );
 }
 export default Custom;
