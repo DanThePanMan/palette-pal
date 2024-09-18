502c78a452cb570e29f9370534879d657a6d00c8
Daniel <danielchenschw@gmail.com>
2024-09-17 23:43:37 -0400

    Complete PreviewLanding Art

--- src/pages/Previews/PreviewPages/PreviewLanding/ColorArt.jsx
+++ src/pages/Previews/PreviewPages/PreviewLanding/ColorArt.jsx
@@ -1,20 +1,17 @@
 import { paletteContext } from "../../../CnR";
 import { useContext } from "react";
+import { convertStyleDiv } from "../../Convert";
 
 function ColorArt() {
     const [palette, setPalette] = useContext(paletteContext);
 
-    function convertStyle(code) {
-        return {
-            backgroundColor: `${code}`,
-        };
-    }
-
     return (
         <div
-            className="hidden md:flex h-64 xl:h-72 w-80 md:w-96 xl:w-[31rem] rounded-2xl border border-neutral-400 transition-all ease-in-out overflow-clip"
-            style={convertStyle(palette.lightShades)}>
-            
+            className="hidden md:grid md:grid-rows-4 md:grid-cols-3 h-64 xl:h-72 w-80 md:w-96 xl:w-[31rem] rounded-2xl transition-all ease-in-out overflow-clip gap-4">
+            <div className="item row-span-2 col-span-1 rounded-md" style={convertStyleDiv(palette.lightAccent)}></div>
+            <div className="item row-span-2 col-span-2 rounded-md" style={convertStyleDiv(palette.mainBrand)}></div>
+            <div className="item row-span-3 col-span-2 rounded-md" style={convertStyleDiv(palette.darkAccent)}></div>
+            <div className="item row-span-3 col-span-1 rounded-md" style={convertStyleDiv(palette.darkShades)}></div>
         </div>
     );
 }
